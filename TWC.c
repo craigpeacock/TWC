1cae3c1759f81433dba38d5f11394c38a1455203
Craig Peacock <craigp@beyondlogic.org>
2020-04-25 04:32:05 +0100

    Tidy up power stats message

--- TWC.c
+++ TWC.c
@@ -195,11 +195,11 @@ int SendMasterHeartbeat(int fd, uint16_t max_current);
 
 bool DecodePowerStatus(struct POWERSTATUS *PowerStatus)
 {
-	printf("Power stats from TWC%04X: ", bswap_16(PowerStatus->src_TWCID));
-	printf("Total kWh since build: %lukWh, ", (long unsigned int)bswap_32(PowerStatus->totalkWh));
-	printf("Phase A: %dV %.0fA, ", 		bswap_16(PowerStatus->phase_a_volts), (float)PowerStatus->phase_a_current/2);
-	printf("Phase B: %dV %.0fA, ",         bswap_16(PowerStatus->phase_b_volts), (float)PowerStatus->phase_b_current/2);
-	printf("Phase C: %dV %.0fA\r\n\r\n",   bswap_16(PowerStatus->phase_c_volts), (float)PowerStatus->phase_c_current/2);
+	printf("Power statistics from TWC%04X: ", bswap_16(PowerStatus->src_TWCID));
+	printf("Total kWh: %lukWh, ", (long unsigned int)bswap_32(PowerStatus->totalkWh));
+	printf("L1: %dV %.0fA, ",	bswap_16(PowerStatus->phase_a_volts), (float)PowerStatus->phase_a_current/2);
+	printf("L2: %dV %.0fA, ",	bswap_16(PowerStatus->phase_b_volts), (float)PowerStatus->phase_b_current/2);
+	printf("L3: %dV %.0fA\r\n\r\n",	bswap_16(PowerStatus->phase_c_volts), (float)PowerStatus->phase_c_current/2);
 	return(true);
 }
 
